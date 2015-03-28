c9fb0b13ec3a3a285180a536a3db93dff1c9b0bc
Femi Omojola <femi@hipmob.com>
2015-03-27 22:31:59 -0400

    Initial commit of the Hipmob HTML5 PhoneGap Build Sample

---
+++ www/js/index.js
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var _hmc = _hmc || [];
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+	
+	// load Hipmob chat
+	_hmc.push(['app', '2ea7d86854df4ca185af84e68ea72fe1']);
+	_hmc.push(['settings', { 'fullscreen': true }]);
+	(function(){
+	    var hm = document.createElement('script'); hm.type = 'text/javascript'; hm.async = true;
+	    hm.src = 'https://hipmob.s3.amazonaws.com/hipmobchat.min.js';
+	    var b = document.getElementsByTagName('body')[0]; b.appendChild(hm);
+	})();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
